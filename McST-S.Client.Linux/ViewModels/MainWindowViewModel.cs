f7fc74fd9a84cb25e97398df83e529c831ebf4a1
CNXiaoyunmiao1322 <CNXiaoyunmiao1322@CNXIAOYUNMIAO13>
2025-08-30 12:19:02 +0800

    添加项目文件。

---
+++ McST-S.Client.Linux/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace McST_S.Client.Linux.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ McST-S.Client.Win/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace McST_S.Client.Win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
